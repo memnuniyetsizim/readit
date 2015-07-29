161baf54055969ef0b45a6b4854974d6006b0a9e
Engin Vardar <enginvardar@gmail.com>
2015-07-30 02:24:07 +0300

    rss standarts and implementation init

---
+++ app/src/Repository/FeedAdapter/Exception/ParserException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: engin
+ * Date: 30/07/15
+ * Time: 02:04
+ */
+
+namespace App\Repository\FeedAdapter\Exception;
+
+
+class ParserException extends \Exception{
+
+}
