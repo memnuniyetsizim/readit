da46fe0b7ca7e790c8c5667892ca8141f9175537
Mustafa Velioglu <me@mustafavelioglu.com>
2015-08-04 01:02:30 +0300

    feed entity definitions

--- app/src/Entity/Feed.php
+++ app/src/Entity/Feed.php
@@ -3,6 +3,45 @@
 namespace App\Entity;
 
 
-class Feed {
+/**
+ * Represents a single feed
+ *
+ * @package App\Entity
+ */
+class Feed
+{
 
+    /**
+     * Url of the feed's homepage
+     *
+     * @var string
+     */
+    public $homepage;
+
+    /**
+     * Url of the feed source
+     *
+     * @var string
+     */
+    public $feedUrl;
+
+    /**
+     * Last update time of the feed
+     *
+     * @var \DateTime
+     */
+    public $updatedAt;
+
+    /**
+     * Adding time of the feed
+     *
+     * @var \DateTime
+     */
+    public $createdAt;
+
+    /**
+     * Type of the feed it can be one of atom, rss, tweet list or repo
+     * @var string
+     */
+    public $feedType;
 }
